dba3ab884f01951a12b9b0a1df475700a30e381d
OussamaAlouat <al317257@uji.es>
2021-08-01 16:29:02 +0200

    Add main header component

---
+++ components/MainHeader.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="main-header">
+    <a href="#">Portfolio</a>
+    <a href="#">About</a>
+    <a href="#">Contact</a>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style lang="scss">
+.main-header {
+  display: flex;
+  flex-direction: row;
+  justify-content: flex-end;
+  padding: 2rem 4rem;
+  a {
+    margin-right: 8px;
+    text-decoration: none;
+    color: black;
+  }
+
+  a:hover {
+    transform: rotate(15deg);
+  }
+}
+
+</style>
