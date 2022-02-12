69f94e7044fc3adb277a4b1be557b1ff8c897595
OussamaAlouat <al317257@uji.es>
2022-02-12 15:39:31 +0100

    Add footer

---
+++ components/Footer.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="footer">
+    <v-card class="mx-auto" max-width="144">
+
+
+      <v-card-title id="c-tech">Technologies </v-card-title>
+    </v-card>
+
+    <v-card class="mx-auto" max-width="144">
+
+
+      <v-card-title>
+
+      Studies </v-card-title>
+    </v-card>
+
+    <v-card class="mx-auto" max-width="144">
+
+
+      <v-card-title>Experience </v-card-title>
+    </v-card>
+  </div>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style lang="scss">
+.footer {
+  display: flex;
+  justify-content: space-evenly;
+  width: 100%;
+}
+
+</style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,14 +1,19 @@
 <template>
   <div class="h-100">
-    <MainHeader id="app-header" />
+    <main-header id="app-header" />
     <home />
+    <app-footer id="foot"/>
   </div>
 </template>
 
 <script>
 import home from './home.vue'
+
+import MainHeader from '~/components/MainHeader.vue'
+import Footer from '~/components/Footer.vue'
+
 export default {
-  components: { home }
+  components: { home, 'app-footer': Footer, MainHeader }
 }
 </script>
 <style lang="scss">
@@ -19,5 +24,10 @@ body {
 #app-header {
   position: fixed;
   right: 0;
+}
+
+#foot {
+  position: fixed;
+  bottom: 1rem;
 }
 </style>
